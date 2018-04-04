a55c3bc47c4483624cf58b2752eb7361a1f5bd93
BABE <supakorn.wo@ku.th>
2018-04-04 17:20:02 +0700

    Initial Commit

---
+++ multipage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  multipage
+//
+//  Created by Supakornbabe on 4/4/2561 BE.
+//  Copyright © 2561 Supakornbabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
